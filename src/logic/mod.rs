397d76b877eb023eb56a0e0cd93e519e08dd2948
[]----[] <brackets@protonmail.com>
2023-06-23 00:52:04 -0400

    feat(logic): basic turns

--- src/logic/mod.rs
+++ src/logic/mod.rs
@@ -15,10 +15,37 @@ fn advance_unit_initiative(mut query: Query<&mut Unit>, time: Res<Time>) {
     }
 }
 
+pub enum UnitAction {
+    Wait,
+}
+
+#[derive(Resource)]
+pub struct UnitTurn {
+    pub unit: Entity,
+    pub start_position: IVec2,
+    pub end_position: IVec2,
+    pub action: UnitAction,
+}
+
+pub fn apply_turn(
+    mut units: Query<(&mut GridPosition, &mut Unit)>,
+    turn: Option<Res<UnitTurn>>,
+    mut commands: Commands,
+) {
+    if let Some(turn) = turn {
+        if let Ok((mut pos, mut unit)) = units.get_mut(turn.unit) {
+            pos.0 = turn.end_position;
+            unit.initiative = 0.0;
+            commands.remove_resource::<UnitTurn>()
+        }
+    }
+}
+
 pub struct LogicPlugin;
 
 impl Plugin for LogicPlugin {
     fn build(&self, app: &mut App) {
         app.add_system(advance_unit_initiative);
+        app.add_system(apply_turn);
     }
 }
