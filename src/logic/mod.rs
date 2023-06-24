bbdd221f07213f92cd9bc5d54e8ea6f0286bd7f5
[]----[] <brackets@protonmail.com>
2023-06-24 18:51:41 -0400

    Advanced reachable tile calculation

--- src/logic/mod.rs
+++ src/logic/mod.rs
@@ -1,5 +1,11 @@
-use bevy::prelude::*;
-use bevy_ecs_ldtk::{GridCoords, IntGridCell};
+use std::collections::{HashSet, VecDeque};
+
+use bevy::{ecs::system::SystemParam, prelude::*};
+use bevy_ecs_ldtk::IntGridCell;
+use bevy_ecs_tilemap::{
+    helpers::square_grid::neighbors::Neighbors,
+    tiles::{TilePos, TileStorage},
+};
 
 use crate::SelectedUnit;
 
@@ -47,15 +53,69 @@ pub fn apply_turn(
     }
 }
 
-fn get_valid_spaces(
-    mut tiles: Query<(&LogicalTile, &GridCoords, &mut ReachableInfo)>,
-    units: Query<(&GridPosition, &UnitSpeed)>,
+#[derive(SystemParam)]
+struct ReachableTilesParam<'w, 's> {
+    logical_tiles: Query<'w, 's, &'static LogicalTile>,
+    tile_storage: Query<'w, 's, &'static TileStorage>,
+    units: Query<'w, 's, (&'static GridPosition, &'static UnitSpeed)>,
+}
+
+fn get_reachable_tiles(
+    ReachableTilesParam {
+        logical_tiles,
+        tile_storage,
+        units,
+    }: ReachableTilesParam,
+    unit: Entity,
+) -> Option<HashSet<TilePos>> {
+    let mut reachable_tiles = HashSet::new();
+    let (GridPosition(pos), &UnitSpeed(speed)) = units.get(unit).ok()?;
+    let tile_storage = tile_storage.get_single().ok()?;
+    let starting_pos = TilePos {
+        x: pos.x as u32,
+        y: pos.y as u32,
+    };
+    let mut tiles_to_explore = VecDeque::new();
+    tiles_to_explore.push_back((0, starting_pos));
+    reachable_tiles.insert(starting_pos);
+
+    while tiles_to_explore.len() > 0 {
+        let (cost_so_far, checking_pos) = tiles_to_explore.pop_front().unwrap();
+
+        let neighbors =
+            Neighbors::get_square_neighboring_positions(&checking_pos, &tile_storage.size, false);
+
+        for (neighbor_pos, neighbor_tile) in neighbors.iter().filter_map(|neighbor_pos| {
+            Some((
+                neighbor_pos,
+                logical_tiles.get(tile_storage.get(neighbor_pos)?).ok()?,
+            ))
+        }) {
+            let cost = cost_so_far + neighbor_tile.move_cost;
+            if cost <= speed && neighbor_tile.can_move {
+                tiles_to_explore.push_back((cost, *neighbor_pos));
+                reachable_tiles.insert(*neighbor_pos);
+            }
+        }
+        tiles_to_explore
+            .make_contiguous()
+            .sort_by_key(|(cost, _)| *cost);
+    }
+    Some(reachable_tiles)
+}
+
+fn mark_reachable_tiles(
+    reachable_tiles_param: ReachableTilesParam,
+    mut reachable_info: Query<(&TilePos, &mut ReachableInfo)>,
     selected: Res<SelectedUnit>,
 ) {
-    if let Ok((GridPosition(pos), &UnitSpeed(speed))) = units.get(selected.0) {
-        println!("Our pos: {}, {}", pos.x, pos.y);
-        for (logical_tile, GridCoords { x, y }, mut reachable_info) in tiles.iter_mut() {
-            reachable_info.reachable = ((pos.x - x).abs() + (pos.y - y).abs()) <= (speed as i32);
+    let reachable_tiles = get_reachable_tiles(reachable_tiles_param, selected.0);
+    for (tile_pos, mut reachable_info) in reachable_info.iter_mut() {
+        let reachable = reachable_tiles
+            .as_ref()
+            .map_or(false, |r| r.contains(tile_pos));
+        if reachable_info.reachable != reachable {
+            reachable_info.reachable = reachable;
         }
     }
 }
@@ -75,11 +135,17 @@ fn populate_logic_tiles(
     mut commands: Commands,
     tiles: Query<(Entity, &IntGridCell), Added<IntGridCell>>,
 ) {
-    for (entity, cell) in tiles.iter() {
+    for (entity, &IntGridCell { value }) in tiles.iter() {
         commands.entity(entity).insert((
-            LogicalTile {
-                can_move: true,
-                move_cost: 1,
+            match value {
+                2 => LogicalTile {
+                    can_move: true,
+                    move_cost: 1,
+                },
+                _ => LogicalTile {
+                    can_move: false,
+                    move_cost: 0,
+                },
             },
             ReachableInfo { reachable: false },
         ));
@@ -93,7 +159,7 @@ impl Plugin for LogicPlugin {
         app.add_system(advance_unit_initiative);
         app.add_system(apply_turn);
         app.add_system(populate_logic_tiles);
-        app.add_system(get_valid_spaces);
+        app.add_system(mark_reachable_tiles);
         app.register_type::<ReachableInfo>();
     }
 }
